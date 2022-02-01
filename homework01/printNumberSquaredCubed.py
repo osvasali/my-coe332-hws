f0b007526d05a404d2a11767dd3607b8870a8358
Osvaldo Salinas <osvaldonoesalinas@gmail.com>
2022-02-01 02:47:31 -0600

    Create printNumberSquaredCubed.py

---
+++ homework01/printNumberSquaredCubed.py
@@ -0,0 +1,6 @@
+onced = [1,2,3,4,5,6,7,8,9,10]
+squared = [1,4,9,16,25,36,49,64,81,100]
+cubed = [1,8,27,64,125,216,343,512,729,1000]
+
+for number in range(10):
+    print(onced[number], squared[number], cubed[number])
